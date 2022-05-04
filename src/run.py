7c2f1c2273c7058e2355bd5effd3e51ae5cdbd62
Amine Neggazi <neggazimedlamine@gmail.com>
2022-05-04 02:49:24 +0100

    Added some useful comments.

--- src/run.py
+++ src/run.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 import model, data
 
 
+# train the model
 def train(model_name):
     train_img_files = glob.glob('data/train/*.jpg')
     test_img_files = glob.glob('data/test/*.jpg')
@@ -22,6 +23,7 @@ def train(model_name):
                 workers=8)
 
 
+# extract number of image chips for an image
 def get_sizes(img,
               offset=150,
               input=188,
@@ -29,6 +31,7 @@ def get_sizes(img,
     return [(len(np.arange(offset, img[0].shape[0] - input / 2, output)), len(np.arange(offset, img[0].shape[1] - input / 2, output)))]
 
 
+# reshape numpy arrays
 def reshape(img,
             size_x,
             size_y,
@@ -41,10 +44,12 @@ def reshape(img,
         print(f'Invalid type: {type} (input, output)')
 
 
+# concatenate images
 def concat(imgs):
     return cv2.vconcat([cv2.hconcat(im_list) for im_list in imgs[:,:,:,:,:]])
 
 
+# predict (segment) image and save a sample output
 def predict(image = glob.glob('data/test/Im037_0.jpg')):
     do_unet = model.get_do_unet()
 
@@ -52,24 +57,27 @@ def predict(image = glob.glob('data/test/Im037_0.jpg')):
     if not os.path.exists(f'models/Test_scale_best.h5'):
         train('Test_scale')
 
-    # Load best weights
+    # load best weights
     do_unet.load_weights(f'models/Test_scale_best.h5')
 
+    # load data
     img, mask, edge = data.load_data(image, padding=200)
     img_chips, mask_chips, edge_chips = data.test_chips(img, mask, edge=edge, padding=100)
 
+    # segment all image chips
     output = do_unet.predict(img_chips)
     new_mask_chips = np.array(output[0])
     new_edge_chips = np.array(output[1])
 
+    # reshape chips arrays to be concatenated
     new_mask_chips = reshape(new_mask_chips, get_sizes(img)[0][0], get_sizes(img)[0][1], 'output')
     new_edge_chips = reshape(new_edge_chips, get_sizes(img)[0][0], get_sizes(img)[0][1], 'output')
 
+    # concatenate chips into a single image (mask and edge)
     new_mask = concat(new_mask_chips)
     new_edge = concat(new_edge_chips)
 
-    plt.imsave('mask.png', new_mask)
-
+    # organize results into one figure
     fig = plt.figure(figsize=(25, 12), dpi=80)
     fig.subplots_adjust(hspace=0.1, wspace=0.1)
     ax = fig.add_subplot(2, 3, 1)
@@ -88,9 +96,11 @@ def predict(image = glob.glob('data/test/Im037_0.jpg')):
     ax.set_title('Predicted edge')
     ax.imshow(new_edge)
 
+    # save the figure as a sample output
     plt.savefig('sample.png')
 
 
+# evaluate model accuracies (mask accuracy and edge accuracy)
 def evaluate():
     train_img_files = glob.glob('data/train/*.jpg')
     test_img_files = glob.glob('data/test/*.jpg')
@@ -101,25 +111,27 @@ def evaluate():
     if not os.path.exists(f'models/Test_scale_best.h5'):
         train('Test_scale')
 
-    # Load best weights
+    # load best weights
     do_unet.load_weights(f'models/Test_scale_best.h5')
 
+    # load data
     img, mask, edge = data.load_data(test_img_files)
     img_chips, mask_chips, edge_chips = data.test_chips(img, mask, edge=edge)
+
+    # print the evaluated accuracies
     print(do_unet.evaluate(img_chips, (mask_chips, edge_chips)))
 
 
-def threshold(img = 'mask.png'):
+# threshold image using otsu's threshold
+def threshold(img = 'output/edge.png'):
     img = cv2.imread(img)
 
-    otsu_threshold, img = cv2.threshold(img, 0, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    otsu_threshold, img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU,)
 
-    plt.imsave('threshold_mask.png', img)
-
-
+    plt.imsave('output/threshold_edge.png', img)
 
 
 if __name__ == '__main__':
     predict()
     threshold()
-
